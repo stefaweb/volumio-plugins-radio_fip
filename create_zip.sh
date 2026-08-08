e7247bb95560eca48396b7002c241d78185b6793
Stephane Leclerc <sleclerc@actionweb.fr>
2026-08-08 16:39:19 +0200

    Update create_zip.sh

--- create_zip.sh
+++ create_zip.sh
@@ -1,6 +1,14 @@
-zip -r radio_fip.zip radio_fip \
+rm -f radio_fip.zip
+
+cd radio_fip
+
+zip -r ../radio_fip.zip . \
+-x ".DS_Store" \
 -x "*/.DS_Store" \
 -x "__MACOSX/*" \
 -x "*/._*" \
--x ".git/*" \
--x "*/.git/*"
+-x ".git/*"
+
+cd ..
+
+unzip -l radio_fip.zip
